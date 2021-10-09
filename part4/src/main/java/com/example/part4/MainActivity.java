78498e79903f4641586b02dc030e3a99895af14e
cuongSkedulo <cuong.nguyen@skedulo.com>
2021-10-09 23:03:22 +0700

    Done part 4

--- part4/src/main/java/com/example/part4/MainActivity.java
+++ part4/src/main/java/com/example/part4/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.part4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,21 +18,57 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = (ListView)findViewById(R.id.list);
 
-        //
+//        Use object as an example
 
-        UserAccount tom = new UserAccount("Tom","admin");
-        UserAccount jerry = new UserAccount("Jerry","user");
-        UserAccount donald = new UserAccount("Donald","guest", false);
+//        UserAccount tom = new UserAccount("Tom","admin");
+//        UserAccount jerry = new UserAccount("Jerry","user");
+//        UserAccount donald = new UserAccount("Donald","guest", false);
+//
+//        UserAccount[] users = new UserAccount[]{tom,jerry, donald};
+//
+//
+//        // android.R.layout.simple_list_item_1 is a constant predefined layout of Android.
+//        // used to create a ListView with simple ListItem (Only one TextView).
+//
+//        ArrayAdapter<UserAccount> arrayAdapter
+//                = new ArrayAdapter<UserAccount>(this, android.R.layout.simple_list_item_1 , users);
+//
+//        listView.setAdapter(arrayAdapter);
 
-        UserAccount[] users = new UserAccount[]{tom,jerry, donald};
+        String[] values = new String[] {
+                "Android List View",
+                "Adapter View",
+                "haha",
+                "hah1",
+                "hah2",
+                "haha3",
+                "haha4",
+                "haha5",
+                "haha6",
+                "haha7",
+                "haha7",
+                "haha7",
+                "haha7",
+                "haha7",
+                "haha7",
+                "haha7",
+                "haha7",
+                "haha7",
+                "haha7",
+                "haha7",
+                "haha8"
+        };
 
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values);
 
-        // android.R.layout.simple_list_item_1 is a constant predefined layout of Android.
-        // used to create a ListView with simple ListItem (Only one TextView).
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value = listView.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this,value, Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        ArrayAdapter<UserAccount> arrayAdapter
-                = new ArrayAdapter<UserAccount>(this, android.R.layout.simple_list_item_1 , users);
-
-        listView.setAdapter(arrayAdapter);
     }
 }
