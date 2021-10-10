d35ee8efb1047c660bd0acce503cd9fb8bc955fc
cuongSkedulo <cuong.nguyen@skedulo.com>
2021-10-10 10:30:12 +0700

    Cannot hide keyboard window with soft window hidden

---
+++ exercise1/src/main/java/com/example/excercise1/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.excercise1;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button save = (Button) findViewById(R.id.saveButton);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+
+                alertDialogBuilder
+                        .setTitle("Do you want to apply for scholarship?")
+                        .setMessage("Click OK to apply for scholarship")
+                        .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                openForm();
+                            }
+                        })
+                        .setPositiveButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+        });
+    }
+
+    private void openForm (){
+        EditText name = (EditText) findViewById(R.id.nameInput);
+        Intent intent = new Intent(MainActivity.this, StudentInfor1.class);
+        intent.putExtra("name", name.getText().toString());
+
+        startActivityForResult(intent, 1);
+    }
+}
---
+++ exercise1/src/main/java/com/example/excercise1/StudentInfor1.java
@@ -0,0 +1,74 @@
+package com.example.excercise1;
+
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class StudentInfor1 extends AppCompatActivity {
+    final Calendar myCalendar = Calendar.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_infor);
+
+        EditText name = (EditText) findViewById(R.id.nameInput);
+
+        Intent intent = getIntent();
+        if (intent.getExtras() != null) {
+            name.setText((String) intent.getExtras().get("name"));
+        }
+
+
+        EditText editText = (EditText) findViewById(R.id.ageInput);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        editText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick (View v){
+                // TODO Auto-generated method stub
+                new DatePickerDialog(StudentInfor1.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setTextIsSelectable(true);
+
+    }
+
+    private void updateLabel() {
+
+        EditText editText = (EditText) findViewById(R.id.ageInput);
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        editText.setText(sdf.format(myCalendar.getTime()));
+    }
+}
